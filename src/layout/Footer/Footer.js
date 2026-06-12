55df5e803899cd4c03c45ee5f3cbf8af42bcc024
Oussema Kachti <oussema.kachti@esprit.tn>
2026-06-12 16:55:01 +0100

    fix: fix company number in footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -122,7 +122,7 @@ const Footer = ({ footerLight, style, footerGradient, translations }) => {
                       <div className="icon-box"><FaPhoneAlt /></div>
                       <div className="text-box">
                         <strong>{t.phone}</strong>
-                        <a href="tel:+216 27 224 949">+216 71 234 567</a>
+                        <a href="tel:+216 27 224 949">+216 27 224 949</a>
                       </div>
                     </li>
                   </ul>
